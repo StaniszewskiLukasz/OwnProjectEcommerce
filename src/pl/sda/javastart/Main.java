8f886221df5a7d2b2f67804ba2e7e399bb208cc3
Łukasz Staniszewski <2277lukas@gmail.com>
2019-03-03 19:32:07 +0100

    Zabawa z Gitem

---
+++ src/pl/sda/javastart/Main.java
@@ -0,0 +1,8 @@
+package pl.sda.javastart;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Tutaj kiedyś coś powstanie. Na razie jest to test ");
+    }
+}
